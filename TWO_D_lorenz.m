732a06c97966171262c73c9a4cb0c03569181856
JITHIN K C <jithinkc22@gmail.com>
2020-05-20 17:12:18 +0530

    Add files via upload

---
+++ TWO_D_lorenz.m
@@ -0,0 +1,15 @@
+function out=TWO_D_lorenz(x,y,h,siz)
+n=siz*siz;
+
+for i=1:n
+
+    % if you need complete code   contact me
+    
+end
+
+x1= reshape(x1,[siz,siz]);
+
+y1= reshape(y1,[siz,siz]);
+%plot(x1,y1,'DisplayName','y1');
+out=x1;
+end
